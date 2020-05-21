e9ef19663163e901ec7ea47f312470ed14abf00b
Tokuro <tokuro@outlook.com>
2020-05-21 17:30:28 +0100

    remove abs, add petg

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1256,8 +1256,8 @@
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED     80
+#define PREHEAT_2_TEMP_HOTEND 228
+#define PREHEAT_2_TEMP_BED     73
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 /**
